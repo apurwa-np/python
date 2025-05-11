136ce91ee829443529b87626ec26fca945e0d59a
apurwa <apurwa@gmail.com>
2025-05-11 14:28:20 +0545

    new feature branch

--- app.py
+++ app.py
@@ -8,4 +8,10 @@ def webout():
 
  return '<h1>DevOps is so much fun to learn.<br>All you need is some linux knowledge.<br>This is great !</h1>'
 
+@app.route('/greet')
+
+def weboutg():
+
+ return '<h1>Hey, DevOps is so much fun to learn.<br>All you need is some linux knowledge.<br>This is great !</h1>'
+
 app.run(host='0.0.0.0',port=7000)
